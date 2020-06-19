c458c3c9cf3e1f5563cc023bd19e1ee77410727c
arrudaluisfp <41311274+arrudaluisfp@users.noreply.github.com>
2020-06-18 23:17:40 -0300

    Update estabelecimento.dart
    Novos campos que foram adicionados ao bando de dados

--- lib/model/estabelecimento.dart
+++ lib/model/estabelecimento.dart
@@ -2,11 +2,28 @@ class Estabelecimento {
   String _nome;
   String _cidade;
   String _foto;
+  String _descricao;
+  String _telefone;
+  String _endereco;
+  String _categoria;
 
 
-  Estabelecimento(this._nome, this._cidade, this._foto);
+  Estabelecimento(this._nome, this._cidade, this._foto, this._descricao, this._telefone, this._endereco, this._categoria);
 
   String get nome => _nome;
+
+
+  String get telefone => _telefone;
+
+  set telefone(String value) {
+    _telefone = value;
+  }
+
+  String get descricao => _descricao;
+
+  set descricao(String value) {
+    _descricao = value;
+  }
 
   set nome(String value) {
     _nome = value;
@@ -22,6 +39,18 @@ class Estabelecimento {
 
   set cidade(String value) {
     _cidade = value;
+  }
+
+  String get endereco => _endereco;
+
+  set endereco(String value) {
+    _endereco = value;
+  }
+
+  String get categoria => _categoria;
+
+  set categoria(String value) {
+    _categoria = value;
   }
 
 
